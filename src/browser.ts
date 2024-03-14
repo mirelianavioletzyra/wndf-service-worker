e9e2ff2b6a63a7bae8aa3fdcbcab7a1e3c3be72d
Tom Mount <tmountjr@gmail.com>
2024-03-14 14:58:08 -0400

    update sw path

--- src/browser.ts
+++ src/browser.ts
@@ -1,27 +1,8 @@
 import { install } from '@edgio/prefetch/window'
-import { prefetch } from '@edgio/prefetch/window/prefetch'
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const { host } = window.location
   install({
+    serviceWorkerPath: '/proxied-service-worker.js',
     includeCacheMisses: true,
-    // watch: [
-    //   {
-    //     // Prefetch any <a href="/..."></a> tags
-    //     selector: `a[href="https://${host}"]`,
-    //     callback: (el) => {
-    //       const href = el.getAttribute('href')
-    //       if (href) prefetch(href)
-    //     }
-    //   },
-    //   {
-    //     // Prefetch any <link href="/..."> tags
-    //     selector: `link[href^="https://${host}/"]`,
-    //     callback: (el) => {
-    //       const href = el.getAttribute('href')
-    //       if (href) prefetch(href)
-    //     },
-    //   }
-    // ]
   })
 })
